85f25337edbcf7ff93e191e7183f0fd5eaa8ded9
Mahjoubech <charkaouielmahjoub50@gmail.com>
2024-10-30 11:55:58 +0100

    Creation Design with Html & TailwindCss : 1 - i creat Header contains logo , Title h1 and input for search 2 - i creat Menu on left side this menu contains plus choise in dev ,this dev 3 - i creat dev for put in tasks cards 4 - i add dev inside parent dev for put title evryTasksCadr (to do,doing,done) 5 - and creat dev for cards and bottund add items 6 - i use for my devs flix-box and grid-box for design my site 7 - i creat formule Ajout contains (tittle,description,option status,date,option periority ) 8 - if user click add items i use js for this i creat function open and function close for hidden and flex form Ajout 9 - finnally , i have a problem need to fixed

---
+++ main.js
@@ -0,0 +1,8 @@
+let Ajout = document.getElementById('Ajoute')
+function closeModal() {
+    Ajoute.style.display = 'none';
+}
+
+function openAjout() {
+    Ajoute.style.display = 'flex';
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./page/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customPurple: '#AB51E3',
+        lightPurple: '#D6B2F3',
+        darkPurple: '#4B007A',
+        colorpur : '#B977E1',
+        P0color : '#F0BFBF',
+        Butn : '#4F1B70',
+        cardsColor:'#8B2FC9',
+        descpcolor : '#E0D4D4',
+      },
+      fontFamily: {
+        quicksand: ['Quicksand', 'sans-serif'],
+      },
+
+ 
+    },
+  },
+  plugins: [],
+}
+
